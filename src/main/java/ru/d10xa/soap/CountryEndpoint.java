7ec158ba5038e1e55ad03edda22f8d15a0985822
d10xa <d10xa@mail.ru>
2015-09-03 12:30:46 +0300

    init

---
+++ src/main/java/ru/d10xa/soap/CountryEndpoint.java
@@ -0,0 +1,30 @@
+package ru.d10xa.soap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import ru.d10xa.schema.bad_practice.countries_service.GetCountryRequest;
+import ru.d10xa.schema.bad_practice.countries_service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+   private static final String NAMESPACE_URI = "http://d10xa.ru/schema/bad-practice/countries-service";
+
+   private CountryRepository countryRepository;
+
+   @Autowired
+   public CountryEndpoint(CountryRepository countryRepository) {
+      this.countryRepository = countryRepository;
+   }
+
+   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+   @ResponsePayload
+   public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+      GetCountryResponse response = new GetCountryResponse();
+      response.setCountry(countryRepository.findCountry(request.getName()));
+
+      return response;
+   }
+}
